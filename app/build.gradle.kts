a96343be2af69dded842469622fe60c80ce691e4
andersbor <anbo@zealand.dk>
2025-02-23 09:27:18 +0100

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.datepickerdialogexample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.datepickerdialogexample"
--- app/src/main/java/com/example/datepickerdialogexample/MainActivity.kt
+++ app/src/main/java/com/example/datepickerdialogexample/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,44 +24,51 @@ import com.example.datepickerdialogexample.ui.theme.DatePickerDialogExampleTheme
 import java.text.DateFormat
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DatePickerDialogExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    var openDialog by remember { mutableStateOf(false) }
-                    var selectedDate by remember { mutableStateOf<Long?>(null) }
-                    val dateFormatterLocal = DateFormat.getDateInstance()
-                    Button(
-                        onClick = { openDialog = true },
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Text(
-                            if (selectedDate != null) dateFormatterLocal.format(selectedDate)
-                            else "Show Date Picker Dialog"
-                        )
-                    }
-                    if (openDialog) {
-                        val datePickerState = rememberDatePickerState()
-                        DatePickerDialog(
-                            onDismissRequest = { /* no reaction */ },
-                            confirmButton = {
-                                TextButton(onClick = {
-                                    openDialog = false
-                                    selectedDate = datePickerState.selectedDateMillis
-                                })
-                                { Text("OK") }
-                            },
-                            dismissButton = {
-                                TextButton(onClick = { openDialog = false })
-                                { Text("Cancel") }
-                            }) {
-                            DatePicker(state = datePickerState)
-                        }
-                    }
-                }
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun MainScreen() {
+    var isDatePickerDialogOpen by remember { mutableStateOf(false) }
+    var selectedDate by remember { mutableStateOf<Long?>(null) }
+    val dateFormatterLocal = DateFormat.getDateInstance()
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Button(
+            onClick = { isDatePickerDialogOpen = true },
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(
+                if (selectedDate != null) dateFormatterLocal.format(selectedDate)
+                else "Show Date Picker Dialog"
+            )
+        }
+        if (isDatePickerDialogOpen) {
+            val datePickerState = rememberDatePickerState()
+            DatePickerDialog(
+                onDismissRequest = { /* no reaction */ },
+                confirmButton = {
+                    TextButton(onClick = {
+                        isDatePickerDialogOpen = false
+                        selectedDate = datePickerState.selectedDateMillis
+                    })
+                    { Text("OK") }
+                },
+                dismissButton = {
+                    TextButton(onClick = { isDatePickerDialogOpen = false })
+                    { Text("Cancel") }
+                }) {
+                DatePicker(state = datePickerState)
             }
         }
     }
